a36859bd45e7e3ba29083bbcb87b980678db27d6
K. Biegert <67793262+kbiegert@users.noreply.github.com>
2023-03-29 15:08:39 -0400

    add working directory to default flavor

---
+++ defaultflavor/workingdirectory/variables.tf
@@ -0,0 +1,44 @@
+
+
+variable "testObject" {
+  description = "This is a test variable of type object."
+  type        = object({
+      key2 = string
+    })
+}
+
+variable "testString" {
+  description = "This is a test variable of type string."
+  type        = string
+}
+
+variable "testNumberInt" {
+  description = "This is a test variable of type integer."
+  type        = number
+}
+
+variable "testNumberFloat" {
+  description = "This is a test variable of type float."
+  type        = number
+}
+
+variable "testBool" {
+  description = "This is a test variable of type bool."
+  type        = bool
+}
+
+variable "testList" {
+  description = "This is a test variable of type list."
+  type        = list(number)
+}
+
+variable "testMap" {
+  description = "This is a test variable of type map."
+  type        = map(string)
+}
+
+variable "testPassword" {
+  description = "This is a test password with no default value"
+  type        = string
+  sensitive   = true
+}
