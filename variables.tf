738246340ed62851fd558a3816299c2bf073557e
K. Biegert <67793262+kbiegert@users.noreply.github.com>
2023-06-22 12:56:54 -0400

    install type testing

--- variables.tf
+++ variables.tf
@@ -1,51 +1,10 @@
-
-
-variable "testObject" {
-  description = "This is a test variable of type object."
-  type        = object({
-      key2 = string
-    })
-}
-
-variable "testString" {
-  description = "This is a test variable of type string."
+variable "echoVersionValue" {
+  description = "Use this to echo a version."
   type        = string
 }
 
-variable "testNumberInt" {
-  description = "This is a test variable of type integer - not required in tf"
-  type        = number
-  default     = 1
-}
-
-variable "testNumberFloat" {
-  description = "This is a test variable of type float."
-  type        = number
-}
-
-variable "testBool" {
-  description = "This is a test variable of type bool."
-  type        = bool
-  default = false
-}
-
-variable "testList" {
-  description = "This is a test variable of type list."
-  type        = list(number)
-}
-
-variable "testMap" {
-  description = "This is a test variable of type map."
-  type        = map(string)
-}
-
-variable "testPassword" {
-  description = "This is a test password with no default value"
+variable "echoInstallType" {
+  description = "Use this to echo an install type - setting to FULLSTACK"
   type        = string
-  sensitive   = true
-}
-
-variable "testOptions" {
-  description = "This is a test options array"
-  type = string
+  default     = "FULLSTACK"
 }
