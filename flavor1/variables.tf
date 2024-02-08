28907de5d19ce1ad0255fac0105e7b20cc9e5e45
K. Biegert <67793262+kbiegert@users.noreply.github.com>
2024-02-08 12:11:36 -0500

    add test flavor one dir from root

---
+++ flavor1/variables.tf
@@ -0,0 +1,11 @@
+variable "echoVersionValue" {
+  description = "Use this to echo a version."
+  type        = string
+  default     = "B"
+}
+
+variable "echoInstallType" {
+  description = "Use this to echo an install type - setting to FULLSTACK"
+  type        = string
+  default     = "EXTENSION"
+}
