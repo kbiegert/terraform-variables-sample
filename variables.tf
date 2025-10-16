bfb64110cad0ad7d8a1de8330a0eeccb45109b68
K. Biegert <67793262+kbiegert@users.noreply.github.com>
2025-10-16 12:00:52 -0400

    set null as default values

--- variables.tf
+++ variables.tf
@@ -1,18 +1,22 @@
 variable "testNumber" {
   description = "This is a test variable of number."
   type        = number
+  default     = null
 }
 variable "testNumberInt" {
   description = "This is a test variable of type integer."
   type        = number
+  default     = null
 }
 
 variable "testNumberFloat" {
   description = "This is a test variable of type float."
   type        = number
+  default     = null
 }
 
 variable "testBool" {
   description = "This is a test variable of type bool."
   type        = bool
+  default     = null
 }
